d95dc6fcaf3bf1d95f6ff1300bf252af063f6e0b
DaviLucasAccount <davilucas2011oficial@gmail.com>
2026-04-04 14:25:37 -0300

    structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# SDLStarterPack
+# Pixelate
+
+cmake -S . -B build
+cmake --build build
+cd build/bin && SDLStarterPack.exe && cd ../..
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+#include <SDL3/SDL.h>
+
+int main(int argc, char *argv[])
+{
+    SDL_Log("working!\n");
+
+    return EXIT_SUCCESS;
+}
